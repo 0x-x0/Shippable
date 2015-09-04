a1b61d55ea936d31a543fe0c9f58b52e8bd934ba
Chetan Tarale <tchetan@snapchai.com>
2015-09-04 11:01:48 +0530

    solution using GitHub API

--- git.py
+++ git.py
@@ -44,38 +44,36 @@ def result():
 	count = count1 = 0
 
 	try:
-		r = requests.get(url,auth=('chetantarale', '2mm10cs009'))
+		r = requests.get(url,auth=('username', 'pass'))
 		print r
 		if(r.ok):
 			repoItem = json.loads(r.text or r.content)
+			#get no of open issues
 			open_issues = repoItem['open_issues']
 			
-			#print open_issues
+			#calculate no of pages as ony 30 results are returned per page
 			noOfPages = float(open_issues)/30
 			intNoOfpages = int(noOfPages)
-
 			if noOfPages > intNoOfpages:
 				noOfPages = intNoOfpages+1
 			elif noOfPages == intNoOfpages:
 				noOfPages = intNoOfpages
 			
-			#print noOfPages
-			
+			#request reach page one by one 
 			for i in range(1,noOfPages+1):
-				#print 'https://api.github.com/repos/Shippable/support/issues?state=open&page='+ str(i)
-				#print urlToIssues + str(i)
-				pr = requests.get(urlToIssues + str(i),auth=('chetantarale', '2mm10cs009'))
-				print pr
+				pr = requests.get(urlToIssues + str(i),auth=('username', 'pass'))
 				res = json.loads(pr.text or pr.content)
+
+				#get timestamp for each issue and append it to list 
 				for i in range(len(res)):
 					dates.append(str(res[i]['created_at'].replace("T", " ").replace("Z", " ")))
 		
-		#print len(dates)
+
 		# convert each timestamp into date object
 		for i in range(len(dates)):
 			date_object = datetime.strptime(dates[i][:-1], '%Y-%m-%d %H:%M:%S')
 			date_o.append(date_object)
-		#print date_o
+
 		#for every date in date_o list check the range
 		for date in date_o:
 			#check if the date is within 24 hours
@@ -85,15 +83,11 @@ def result():
 			if date < last24hrs and date > last7days:
 				count1 = count1 + 1
 						 
-
+		#only string can be returned hence sonvert them to string
 		within24 = str(count)
 		within7 = str(count1)
 		after7 = str(open_issues - count1 - count)
 
-		print open_issues
-		print within24
-		print within7
-		print after7
 		return render_template('result.html', issuesOpen=open_issues,within24 = within24,within7 = within7, after7 = after7)
 	except:
 		return render_template('error.html')
